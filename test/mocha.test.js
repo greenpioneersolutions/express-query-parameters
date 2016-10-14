693e1ddf70b41ddd313bfe236dfc639f6202fc34
Jason Humphrey <Humphrey.jason32@gmail.com>
2016-10-14 16:17:19 -0500

    update the name & future options

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var app = express()
-var build = require('..')({
+var queryParameters = require('..')({
   settings: {
     schema: ['name', 'title', 'content', 'roles', 'email'],
     adapter: 'mongoose'
@@ -10,7 +10,7 @@ var build = require('..')({
   //             object|string:supported adapter
   }
 })
-app.use(build.middleware())
+app.use(queryParameters.middleware())
 app.get('/query', function (req, res, next) {
   res.send(req.queryParameters)
 })
@@ -22,93 +22,93 @@ var _ = require('lodash')
 describe('Express Query Parameters', function () {
   describe('General', function () {
     it('Config', function () {
-      var config = build.config()
+      var config = queryParameters.config()
       var defaults = _.cloneDeep(config.query)
-      _.merge(defaults, build.adapter.options)
-      assert.deepEqual(build.parse(), defaults)
+      _.merge(defaults, queryParameters.adapter.options)
+      assert.deepEqual(queryParameters.parse(), defaults)
     })
     it('Filter', function () {
-      assert.deepEqual(build.parse({name: 'jason'}).filter, {
+      assert.deepEqual(queryParameters.parse({name: 'jason'}).filter, {
         name: 'jason'
       })
     })
     it('Lean', function () {
-      assert.equal(build.parse({lean: 'true'}).lean, true)
-      assert.equal(build.parse({lean: true}).lean, true)
+      assert.equal(queryParameters.parse({lean: 'true'}).lean, true)
+      assert.equal(queryParameters.parse({lean: true}).lean, true)
     })
     it('Skip', function () {
-      assert.equal(build.parse({skip: '4'}).skip, 4)
-      assert.equal(build.parse({skip: 4}).skip, 4)
+      assert.equal(queryParameters.parse({skip: '4'}).skip, 4)
+      assert.equal(queryParameters.parse({skip: 4}).skip, 4)
     })
     it('Sort', function () {
-      assert.equal(build.parse({sort: 'name'}).sort, 'name')
+      assert.equal(queryParameters.parse({sort: 'name'}).sort, 'name')
     })
     it('Limit', function () {
-      assert.equal(build.parse({limit: '2'}).limit, 2)
-      assert.equal(build.parse({limit: 2}).limit, 2)
+      assert.equal(queryParameters.parse({limit: '2'}).limit, 2)
+      assert.equal(queryParameters.parse({limit: 2}).limit, 2)
     })
     it('Select', function () {
-      assert.deepEqual(build.parse({select: 'name'}).select, { name: 1 })
-      assert.deepEqual(build.parse({select: 'name title'}).select, { name: 1, title: 1 })
+      assert.deepEqual(queryParameters.parse({select: 'name'}).select, { name: 1 })
+      assert.deepEqual(queryParameters.parse({select: 'name title'}).select, { name: 1, title: 1 })
     })
   })
   describe('Mongoose - Parse', function () {
     it('gt', function () {
-      assert.equal(build.parse({where: 'name', gt: '2016-10-20'}).gt, '2016-10-20')
+      assert.equal(queryParameters.parse({where: 'name', gt: '2016-10-20'}).gt, '2016-10-20')
     })
     it('lt', function () {
-      assert.equal(build.parse({where: 'name', lt: '2016-10-20'}).lt, '2016-10-20')
+      assert.equal(queryParameters.parse({where: 'name', lt: '2016-10-20'}).lt, '2016-10-20')
     })
     it('gte', function () {
-      assert.equal(build.parse({where: 'name', gte: '2016-10-20'}).gte, '2016-10-20')
+      assert.equal(queryParameters.parse({where: 'name', gte: '2016-10-20'}).gte, '2016-10-20')
     })
     it('lte', function () {
-      assert.equal(build.parse({where: 'name', lte: '2016-10-20'}).lte, '2016-10-20')
+      assert.equal(queryParameters.parse({where: 'name', lte: '2016-10-20'}).lte, '2016-10-20')
     })
     it('in', function () {
-      assert.equal(build.parse({where: 'roles', in: 'admin'}).in, 'admin')
+      assert.equal(queryParameters.parse({where: 'roles', in: 'admin'}).in, 'admin')
     })
     it('ne', function () {
-      assert.equal(build.parse({where: 'roles', ne: 'admin'}).ne, 'admin')
+      assert.equal(queryParameters.parse({where: 'roles', ne: 'admin'}).ne, 'admin')
     })
     it('nin', function () {
-      assert.equal(build.parse({where: 'roles', nin: 'admin'}).nin, 'admin')
+      assert.equal(queryParameters.parse({where: 'roles', nin: 'admin'}).nin, 'admin')
     })
     it('regex', function () {
-      assert.equal(build.parse({where: 'name', regex: 'ja'}).regex, 'ja')
+      assert.equal(queryParameters.parse({where: 'name', regex: 'ja'}).regex, 'ja')
     })
     it('options', function () {
-      assert.equal(build.parse({where: 'name', options: '2016-10-20'}).options, '2016-10-20')
+      assert.equal(queryParameters.parse({where: 'name', options: '2016-10-20'}).options, '2016-10-20')
     })
     it('size', function () {
-      assert.equal(build.parse({where: 'roles', size: '3'}).size, '3')
+      assert.equal(queryParameters.parse({where: 'roles', size: '3'}).size, '3')
     })
     it('all', function () {
-      assert.equal(build.parse({where: 'name', all: '2016-10-20'}).all, '2016-10-20')
+      assert.equal(queryParameters.parse({where: 'name', all: '2016-10-20'}).all, '2016-10-20')
     })
     it('or', function () {
-      assert.deepEqual(build.parse({or: {roles: ['admin', 'help']}}).or, { '$or': [ { roles: 'admin' }, { roles: 'help' } ] })
+      assert.deepEqual(queryParameters.parse({or: {roles: ['admin', 'help']}}).or, { '$or': [ { roles: 'admin' }, { roles: 'help' } ] })
     })
     it('nor', function () {
-      assert.deepEqual(build.parse({nor: {roles: ['admin', 'help']}}).nor, { '$nor': [ { roles: 'admin' }, { roles: 'help' } ] })
+      assert.deepEqual(queryParameters.parse({nor: {roles: ['admin', 'help']}}).nor, { '$nor': [ { roles: 'admin' }, { roles: 'help' } ] })
     })
     it('and', function () {
-      assert.deepEqual(build.parse({and: {roles: ['admin', 'help']}}).and, { '$and': [ { roles: 'admin' }, { roles: 'help' } ] })
+      assert.deepEqual(queryParameters.parse({and: {roles: ['admin', 'help']}}).and, { '$and': [ { roles: 'admin' }, { roles: 'help' } ] })
     })
     it('aggregate', function () {
-      assert.deepEqual(build.parse({aggregate: {roles: ['admin', 'help']}}).aggregate, [{roles: ['admin', 'help']}])
+      assert.deepEqual(queryParameters.parse({aggregate: {roles: ['admin', 'help']}}).aggregate, [{roles: ['admin', 'help']}])
     })
     it('populateId', function () {
-      assert.equal(build.parse({populateId: 'name title person'}).populateId, 'name title')
+      assert.equal(queryParameters.parse({populateId: 'name title person'}).populateId, 'name title')
     })
     it('populateItems', function () {
-      assert.equal(build.parse({populateItems: 'name role content'}).populateItems, 'name content')
+      assert.equal(queryParameters.parse({populateItems: 'name role content'}).populateItems, 'name content')
     })
     it('deepPopulate', function () {
-      assert.equal(build.parse({deepPopulate: 'name.test role content.type'}).deepPopulate, 'name.test role content.type')
+      assert.equal(queryParameters.parse({deepPopulate: 'name.test role content.type'}).deepPopulate, 'name.test role content.type')
     })
     it('where - gt & lt', function () {
-      assert.deepEqual(build.parse({where: 'created', gt: '2015-11-17', lt: '2019-12-30'}).where, { created: { '$gt': '2015-11-17', '$lt': '2019-12-30' } })
+      assert.deepEqual(queryParameters.parse({where: 'created', gt: '2015-11-17', lt: '2019-12-30'}).where, { created: { '$gt': '2015-11-17', '$lt': '2019-12-30' } })
     })
   })
   describe('Mongoose - Middleware', function () {
@@ -117,9 +117,9 @@ describe('Express Query Parameters', function () {
         .get('query')
         .expect(200, function (err, res) {
           if (err) return done(err)
-          var config = build.config()
+          var config = queryParameters.config()
           var defaults = _.cloneDeep(config.query)
-          _.merge(defaults, build.adapter.options)
+          _.merge(defaults, queryParameters.adapter.options)
           assert.deepEqual(res.body, defaults)
           done()
         })
