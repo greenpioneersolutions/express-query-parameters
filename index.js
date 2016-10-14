5b7fa9cd50abd4828ef6ed69008049b982f2a479
Jason Humphrey <Humphrey.jason32@gmail.com>
2016-10-14 01:31:44 -0500

    added support for where & mongoose functions

--- index.js
+++ index.js
@@ -61,9 +61,87 @@ function Build (options) {
     }
   }
   self.mongoose = function (query) {
+    var where = {
+      used: false,
+      parameters: {}
+    }
+    function whereParameter (check, type) {
+      if (!_.isUndefined(check)) {
+        where.used = true
+        where.parameters[type] = check
+        return check
+      }
+      return false
+    }
     return {
       lean: function (cb) {
         cb(null, query.lean ? autoParse(query.lean) : self.options.query.lean)
+      },
+      gt: function (cb) {
+        cb(null, query.gt ? whereParameter(query.gt, '$gt') : self.options.mongoose.gt)
+      },
+      lt: function (cb) {
+        cb(null, query.lt ? whereParameter(query.lt, '$lt') : self.options.mongoose.lt)
+      },
+      gte: function (cb) {
+        cb(null, query.gte ? whereParameter(query.gte, '$gte') : self.options.mongoose.gte)
+      },
+      lte: function (cb) {
+        cb(null, query.lte ? whereParameter(query.lte, '$lte') : self.options.mongoose.lte)
+      },
+      in: function (cb) {
+        cb(null, query.in ? whereParameter(query.in, '$in') : self.options.mongoose.in)
+      },
+      ne: function (cb) {
+        cb(null, query.ne ? whereParameter(query.ne, '$ne') : self.options.mongoose.ne)
+      },
+      nin: function (cb) {
+        cb(null, query.nin ? whereParameter(query.nin, '$nin') : self.options.mongoose.nin)
+      },
+      regex: function (cb) {
+        cb(null, query.regex ? whereParameter(query.regex, '$regex') : self.options.mongoose.regex)
+      },
+      options: function (cb) {
+        cb(null, query.options ? whereParameter(query.options, '$options') : self.options.mongoose.options)
+      },
+      size: function (cb) {
+        cb(null, query.size ? whereParameter(query.size, '$size') : self.options.mongoose.size)
+      },
+      all: function (cb) {
+        cb(null, query.all ? whereParameter(query.all, '$all') : self.options.mongoose.all)
+      },
+      equals: function (cb) {
+        cb(null, query.equals ? whereParameter(query.equals, '$equals') : self.options.mongoose.equals)
+      },
+      find: function (cb) {
+        cb(null, query.find ? whereParameter(query.find, 'find') : self.options.mongoose.find)
+      },
+      or: function (cb) {
+        cb(null, query.or ? andOrNorParameter(query.or, '$or') : self.options.mongoose.or)
+      },
+      nor: function (cb) {
+        cb(null, query.nor ? andOrNorParameter(query.nor, '$nor') : self.options.mongoose.nor)
+      },
+      and: function (cb) {
+        cb(null, query.and ? andOrNorParameter(query.and, '$and') : self.options.mongoose.and)
+      },
+      aggregate: function (cb) {
+        cb(null, query.aggregate ? aggregateCheck(query.aggregate) : self.options.mongoose.aggregate)
+      },
+      populateId: function (cb) {
+        cb(null, query.populateId ? populateCheck(query.populateId, self.options, 'populateId') : self.options.mongoose.populateId) // models
+      },
+      populateItems: function (cb) {
+        cb(null, query.populateItems ? populateCheck(query.populateItems, self.options, 'populateItems') : self.options.mongoose.populateItems) // settings.schema
+      },
+      deepPopulate: function (cb) {
+        cb(null, query.deepPopulate ? query.deepPopulate : self.options.mongoose.deepPopulate) // add settings.schema check for deepPop
+      },
+      where: function (cb) {
+        cb(null, query.where || {})
+      },
+      whereParameters: function (cb) {
+        cb(null, where)
       }
     }
   }
@@ -78,16 +156,20 @@ function Build (options) {
 
 Build.prototype.config = function (data) {
   var self = this
-  self.options = _.merge(self.options, data)
+  if (data)self.options = _.merge(self.options, data)
+  return self.options
 }
 Build.prototype.parse = function (parse, cb) {
   var options = this.options
+  var defaults = _.cloneDeep(this.options.query)
+  if (this.options.settings.mongoose) _.merge(defaults, options.mongoose)
+  if (this.options.settings.sql) _.merge(defaults, options.sql)
   var self = this
   if (_.isEmpty(parse)) {
-    if (cb) return cb(self.options.query)
-    return self.options.query
+    if (cb) return cb(defaults)
+    return defaults
   }
-
+  parse = autoParse(parse)
   var tasks = self.tasks(parse)
   if (this.options.settings.mongoose) _.merge(tasks, self.mongoose(parse))
   if (this.options.settings.sql) _.merge(tasks, self.sql(parse))
@@ -95,7 +177,20 @@ Build.prototype.parse = function (parse, cb) {
     tasks
     , function (err, result) {
       if (err) console.log(err)
-      return _.merge(options, result)
+      if (options.settings.mongoose) {
+        if (result.whereParameters.used) {
+          // result.where = result.whereParameters
+          // call function
+          if (!_.isArray(result.where)) {
+            result.where = whereObject(result.where, result.whereParameters.parameters, options.settings.schema)
+          } else {
+            result.where = whereObject(result.where[0] || '', result.whereParameters.parameters, options.settings.schema)
+            console.log('The Where selector appears to be the problem. Looks like we got a multiple wheres but expect a where', 'Where', err)
+          }
+        }
+        delete result.whereParameters
+      }
+      return _.merge(defaults, result)
     })
   if (cb) {
     return cb(response)
@@ -186,21 +281,96 @@ function selectCheck (select, schema) {
   return selected
 }
 
-// function errorHandler (errValue, field, value, queryParameters, defaults) { // ERRORHANDLER
-//   setOrGet(queryParameters.error, field, []).push({
-//     message: defaults.errorMessage,
-//     value: errValue
-//   })
-//   return value
-// }
+function andOrNorParameter (data, type) {
+  if (data) {
+    var queryParameters = {}
+    queryParameters[type] = []
+    _.forEach(data, function (o, key) {
+      _.forEach(o, function (d) {
+        var obj = {}
+        obj[key] = autoParse(d)
+        queryParameters[type].push(obj)
+      })
+    })
+    return queryParameters
+  } else {
+    return false
+  }
+}
 
-// function warningHandler (warnValue, field, value, queryParameters, defaults) { // warningHandler
-//   setOrGet(queryParameters.warning, field, []).push({
-//     message: defaults.errorMessage,
-//     value: errValue
-//   })
-//   return value
-// }
+function aggregateCheck (aggregated) {
+  var output = []
+
+  _.forEach(aggregated, function (n, key) {
+    var aggregate = {}
+    aggregate[key] = autoParse(n) // valueParse(n)
+    output.push(aggregate)
+  })
+  return output
+}
+
+function populateCheck (data, options, type) {
+  var defaults = options.mongoose
+  var output = ''
+  if (!_.isArray(data)) {
+    data = data.replace(',', ' ').split(' ')
+  }
+  var defaultIds = []
+  if (!_.isArray(defaults[type])) {
+    defaultIds = defaults[type].replace(',', ' ').split(' ')
+  }
+  if (defaults[type]) {
+    data = _.union(data, defaultIds)
+  }
+  _.forEach(data, function (n, key) {
+    if (_.includes(options.settings.schema, n)) {
+      output += n + ' '
+    }
+  })
+  return _.trim(output)
+}
+
+function whereObject (where, whereParameters, schema) {
+  var output = {}
+  output[where] = {}
+  _.forEach(whereParameters, function (n, key) {
+    if (key === '$equals') {
+      try {
+        output[where] = autoParse(n) // valueParser(settings.schemas[settings.modelNameCheck(_.replace(req.path, settings.options.routing.url, ''))].paths[where].instance, n)
+      } catch (err) {
+        console.log(where + '- The Where selector appears to be the problem', 'Where$equals', err)
+      }
+    } else if (key === 'find') {
+      if (schema.indexOf(where) !== -1) {
+        output[where] = autoParse(n) // new RegExp(_.escapeRegExp(autoParse(n)), 'i')
+      } else {
+        console.log(where + '- The Where Find is not in the schema - strict mode on ', 'WhereFind', 'err')
+      }
+    } else if (key === '$nin' || key === '$in' || key === '$all') {
+      try {
+        if (_.isArray(n)) {
+          if (schema.indexOf(where) !== -1) {
+            output[where][key] = autoParse(n)
+          } else {
+            console.log(where + '- The Where $nin $in $all is not in the schema - strict mode on ', 'Whereinall', 'err')
+          }
+        } else {
+          if (schema.indexOf(where) !== -1) {
+            output[where][key] = [autoParse(n)]
+          } else {
+            console.log(where + '- The Where $nin $in $all is not in the schema - strict mode on ', 'WhereEinall', 'err')
+          }
+        }
+      } catch (err) {
+        console.log(where + '- The Where selector appears to be the problem', 'Where-all', err)
+      }
+    } else {
+      output[where][key] = autoParse(n)
+    }
+  })
+
+  return output
+}
 
 function build (options) {
   if (options === undefined || (typeof options === 'object' && options !== null)) {
--- test/mocha.test.js
+++ test/mocha.test.js
@@ -1,32 +1,113 @@
-var build = require('../index')({settings: {schema: ['name', 'title', 'content'], mongoose: true}})
-var options = require('../lib/options.js')
+var build = require('../index')({settings: {schema: ['name', 'title', 'content', 'roles'], mongoose: true}})
 var assert = require('chai').assert
+var _ = require('lodash')
 describe('Express Query Parameters', function () {
-  it('Empty', function () {
-    assert.deepEqual(build.parse(), options.query)
-  })
-  it('Filter', function () {
-    assert.deepEqual(build.parse({name: 'jason'}).filter, {
-      name: 'jason'
+  describe('General', function () {
+    it('Config', function () {
+      var config = build.config()
+      var defaults = _.cloneDeep(config.query)
+      if (config.settings.mongoose) _.merge(defaults, config.mongoose)
+      if (config.settings.sql) _.merge(defaults, config.sql)
+      assert.deepEqual(build.parse(), defaults)
+    })
+    it('Filter', function () {
+      assert.deepEqual(build.parse({name: 'jason'}).filter, {
+        name: 'jason'
+      })
+    })
+    it('Lean', function () {
+      assert.equal(build.parse({lean: 'true'}).lean, true)
+      assert.equal(build.parse({lean: true}).lean, true)
+    })
+    it('Skip', function () {
+      assert.equal(build.parse({skip: '4'}).skip, 4)
+      assert.equal(build.parse({skip: 4}).skip, 4)
+    })
+    it('Sort', function () {
+      assert.equal(build.parse({sort: 'name'}).sort, 'name')
+    })
+    it('Limit', function () {
+      assert.equal(build.parse({limit: '2'}).limit, 2)
+      assert.equal(build.parse({limit: 2}).limit, 2)
+    })
+    it('Select', function () {
+      assert.deepEqual(build.parse({select: 'name'}).select, { name: 1 })
+      assert.deepEqual(build.parse({select: 'name title'}).select, { name: 1, title: 1 })
     })
   })
-  it('Lean', function () {
-    assert.equal(build.parse({lean: 'true'}).lean, true)
-    assert.equal(build.parse({lean: true}).lean, true)
+  describe('Mongoose - Parse', function () {
+    it('gt', function () {
+      assert.equal(build.parse({where: 'name', gt: '2016-10-20'}).gt, '2016-10-20')
+    // assert.equal(build.parse({where: 'name', gt: '2016-10-20'}).where, 'name')
+    })
+    it('lt', function () {
+      assert.equal(build.parse({where: 'name', lt: '2016-10-20'}).lt, '2016-10-20')
+    // assert.equal(build.parse({where: 'name', lt: '2016-10-20'}).where, 'name')
+    })
+    it('gte', function () {
+      assert.equal(build.parse({where: 'name', gte: '2016-10-20'}).gte, '2016-10-20')
+    // assert.equal(build.parse({where: 'name', gte: '2016-10-20'}).where, 'name')
+    })
+    it('lte', function () {
+      assert.equal(build.parse({where: 'name', lte: '2016-10-20'}).lte, '2016-10-20')
+    // assert.equal(build.parse({where: 'name', lte: '2016-10-20'}).where, 'name')
+    })
+    it('in', function () {
+      assert.equal(build.parse({where: 'roles', in: 'admin'}).in, 'admin')
+    // assert.equal(build.parse({where: 'roles', in: 'admin'}).where, 'roles')
+    })
+    it('ne', function () {
+      assert.equal(build.parse({where: 'roles', ne: 'admin'}).ne, 'admin')
+    // assert.equal(build.parse({where: 'roles', ne: 'admin'}).where, 'roles')
+    })
+    it('nin', function () {
+      assert.equal(build.parse({where: 'roles', nin: 'admin'}).nin, 'admin')
+    // assert.equal(build.parse({where: 'roles', nin: 'admin'}).where, 'roles')
+    })
+    it('regex', function () {
+      assert.equal(build.parse({where: 'name', regex: 'ja'}).regex, 'ja')
+    // assert.equal(build.parse({where: 'name', regex: 'ja'}).where, 'name')
+    })
+    it('options', function () {
+      assert.equal(build.parse({where: 'name', options: '2016-10-20'}).options, '2016-10-20')
+    // assert.equal(build.parse({where: 'name', options: '2016-10-20'}).where, 'name')
+    })
+    it('size', function () {
+      assert.equal(build.parse({where: 'roles', size: '3'}).size, '3')
+    // assert.equal(build.parse({where: 'roles', size: '3'}).where, 'roles')
+    })
+    it('all', function () {
+      assert.equal(build.parse({where: 'name', all: '2016-10-20'}).all, '2016-10-20')
+    // assert.equal(build.parse({where: 'name', all: '2016-10-20'}).where, 'name')
+    })
+    it('or', function () {
+      assert.deepEqual(build.parse({or: {roles: ['admin', 'help']}}).or, { '$or': [ { roles: 'admin' }, { roles: 'help' } ] })
+    })
+    it('nor', function () {
+      assert.deepEqual(build.parse({nor: {roles: ['admin', 'help']}}).nor, { '$nor': [ { roles: 'admin' }, { roles: 'help' } ] })
+    })
+    it('and', function () {
+      assert.deepEqual(build.parse({and: {roles: ['admin', 'help']}}).and, { '$and': [ { roles: 'admin' }, { roles: 'help' } ] })
+    })
+    it('aggregate', function () {
+      assert.deepEqual(build.parse({aggregate: {roles: ['admin', 'help']}}).aggregate, [{roles: ['admin', 'help']}])
+    })
+    it('populateId', function () {
+      assert.equal(build.parse({populateId: 'name title person'}).populateId, 'name title')
+    })
+    it('populateItems', function () {
+      assert.equal(build.parse({populateItems: 'name role content'}).populateItems, 'name content')
+    })
+    it('deepPopulate', function () {
+      assert.equal(build.parse({deepPopulate: 'name.test role content.type'}).deepPopulate, 'name.test role content.type')
+    })
   })
-  it('Skip', function () {
-    assert.equal(build.parse({skip: '4'}).skip, 4)
-    assert.equal(build.parse({skip: 4}).skip, 4)
+  describe('Mongoose - Parse - Advanced', function () {
+    it('where - gt & lt', function () {
+      assert.deepEqual(build.parse({where: 'created', gt: '2015-11-17', lt: '2019-12-30'}).where, { created: { '$gt': '2015-11-17', '$lt': '2019-12-30' } })
+    })
   })
-  it('Sort', function () {
-    assert.equal(build.parse({sort: 'name'}).sort, 'name')
-  })
-  it('Limit', function () {
-    assert.equal(build.parse({limit: '2'}).limit, 2)
-    assert.equal(build.parse({limit: 2}).limit, 2)
-  })
-  it('Select', function () {
-    assert.deepEqual(build.parse({select: 'name'}).select, { name: 1 })
-    assert.deepEqual(build.parse({select: 'name title'}).select, { name: 1, title: 1 })
+  describe('SQL', function () {
+    // Nothing yet
   })
 })
